a92e405cf578e579b85d39a59ddecca727e76383
mark_harmon <mahmah1962@gmail.com>
2026-03-11 15:44:30 -0500

    got rooms all connected and a map

--- main.py
+++ main.py
@@ -4,42 +4,21 @@ from rooms import *
 def main():
     finished = False
     maze = {0:{"Left": None, "Right": None, "Forward": None}}
-    room_id = 0
-    next_step = False
-    while next_step == False:
-        print("ENTRANCE")
-        show_room(maze[0])
-        users_choice = input().capitalize()
-        print()
-        next_step = go_to_next_room(maze, room_id, users_choice)
-
-
+    current_room = 0
+        
     while finished == False:
-        next_step = False
-        if users_choice != "Back":
-            room = generate_maze()
-            room_id += 1
-            maze[room_id] = room
-            while next_step == False:
-                print(f"ROOM {room_id}")
-                show_room(room)
-                users_choice = input().capitalize()
-                print()
-                next_step = go_to_next_room(maze, room_id, users_choice)
-                
-        else:
-            room_id -= 1
-            pervious_room = maze[room_id]
-            while next_step == False:
-                if room_id == 0:
-                    print("ENTRANCE")
-                else:
-                    print(f"ROOM {room_id}:")
-                show_room(pervious_room)
-                users_choice = input().capitalize()
-                print()
-                next_step = go_to_next_room(maze, room_id, users_choice)
+        show_map(maze, current_room)
+        show_room(maze[current_room])
+        direction = input().capitalize()
 
+        if direction not in maze[current_room]:
+            print("You can't go that way")
+            continue
+        if maze[current_room][direction] is None:
+            new_room_id = new_room(maze, current_room, direction)
+            current_room = new_room_id
+        else:
+            current_room = maze[current_room][direction]
 
 if __name__ == "__main__":
     main()
--- mazes.py
+++ mazes.py
@@ -23,11 +23,36 @@ OPPOSITE = {
 
 
 def generate_maze():
-    room_selector = random.choice(ROOM_TYPES)
+    room_selector = random.choice(ROOM_TYPES).copy()
     return room_selector
 
 
-def generate_room():
-    return random.choice(ROOM_TYPES)
+def new_room(maze, room_id, users_choice):
+    if random.random() < 0.2:
+        new_room = random.choice(list(maze.keys()))
+    else:
+        new_room = len(maze)
+        maze[new_room] = generate_maze()
+        connect(maze, room_id, users_choice, new_room)
+    return new_room
 
 
+def connect(maze,current_room, choice, new_room):
+
+    maze[current_room][choice] = new_room
+    opposite = OPPOSITE[choice]
+    if opposite not in maze[new_room]:
+        maze[new_room][opposite] = None
+    maze[new_room][opposite] = current_room
+
+def show_map(maze, current_room):
+
+    print("\n--- MAP ---")
+
+    for room, paths in maze.items():
+
+        marker = " <YOU>" if room == current_room else ""
+
+        print(f"{room}: {paths}{marker}")
+
+    print("-----------\n")
